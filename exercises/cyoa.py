32956b7b3d14e9976c7942a3fc762b25cd61905e
Edlyn Jeyaraj <ejeyaraj@unc.edu>
2023-03-16 18:51:43 -0400

    progress

--- exercises/cyoa.py
+++ exercises/cyoa.py
@@ -2,24 +2,23 @@
 
 __author__ = "730569503"
 
-if __name__ == "__main__":
-    main()
+points: int = None
+player: str = None
 
 def main()-> None:
     global points
-    points: int = 0
-    global player
-    player: str = input("What is your name?")
+    points = 0
 
     greet()
 
-
-
-
-
-
-
-    __name__ = "__main__"
-
 def greet()-> None:
-    print("")
+    global player
+    player = input("Welcome to PlantCute, where you will be taking care of a virtual plant. What is your name?")
+
+def water_plant()-> None:
+    water_plant_output = 
+
+
+
+if __name__ == "__main__":
+    main()
